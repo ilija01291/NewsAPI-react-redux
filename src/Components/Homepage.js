fc38830b364c7c268d23b3021860afa3db6c0bea
ilija01291 <68786980+ilija01291@users.noreply.github.com>
2021-05-17 20:29:34 +0200

    first commit

---
+++ src/Components/Homepage.js
@@ -0,0 +1,55 @@
+import React from "react";
+import GoogleLogin from "react-google-login";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  selectSignedIn,
+  setSignedIn,
+  setUserData,
+} from "../features/userSlice";
+
+import "../home.css";
+
+const Homepage = () => {
+  const isSignedIn = useSelector(selectSignedIn);
+
+  const dispatch = useDispatch();
+  const login = (response) => {
+    console.log(response);
+    dispatch(setSignedIn(true));
+    dispatch(setUserData(response.profileObj));
+  };
+
+  return (
+    <div className="home__page" style={{ display: isSignedIn ? "none" : "" }}>
+      {!isSignedIn ? (
+        <div className="login__message">
+          <h2>TEHNO News</h2>
+        
+          <p>
+            News about tehnology on one place mate :)
+          </p>
+          <GoogleLogin
+            clientId="905024438999-idc46neh62g57gencd36jvcpuu0udi35.apps.googleusercontent.com"
+            render={(renderProps) => (
+              <button
+                onClick={renderProps.onClick}
+                disabled={renderProps.disabled}
+                className="login__button"
+              >
+                Login with Google
+              </button>
+            )}
+            onSuccess={login}
+            onFailure={login}
+            isSignedIn={true}
+            cookiePolicy={"single_host_origin"}
+          />
+        </div>
+      ) : (
+        ""
+      )}
+    </div>
+  );
+};
+
+export default Homepage;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,66 @@
+import { Avatar } from "@material-ui/core";
+import React, { useState } from "react";
+import { GoogleLogout } from "react-google-login";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  selectSignedIn,
+  selectUserData,
+  setInput,
+  setSignedIn,
+  setUserData,
+} from "../features/userSlice";
+
+import "../navbar.css";
+
+const Navbar = () => {
+  const [inputValue, setInputValue] = useState();
+  const isSignedIn = useSelector(selectSignedIn);
+  const userData = useSelector(selectUserData);
+
+  const dispatch = useDispatch();
+
+  const logout = (response) => {
+    dispatch(setSignedIn(false));
+    dispatch(setUserData(null));
+  };
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    dispatch(setInput(inputValue));
+  };
+
+  return (
+    <div className="navbar">
+      <h1 className="navbar__header">Tehnology news</h1>
+      
+
+      {isSignedIn ? (
+        <div className="navbar__user__data">
+          <Avatar
+            className="user"
+            src={userData?.imageUrl}
+            alt={userData?.name}
+          />
+          <h1 className="signedIn">{userData?.givenName}</h1>
+          <GoogleLogout
+            clientId="905024438999-idc46neh62g57gencd36jvcpuu0udi35.apps.googleusercontent.com"
+            render={(renderProps) => (
+              <button
+                onClick={renderProps.onClick}
+                disabled={renderProps.disabled}
+                className="logout__button"
+              >
+                Logout 😦
+              </button>
+            )}
+            onLogoutSuccess={logout}
+          />
+        </div>
+      ) : (
+        <h1 className="notSignedIn">User not available 😞</h1>
+      )}
+    </div>
+  );
+};
+
+export default Navbar;
